cdab02c163ac9115f65166fcbaade4ddfbea78a6
TheRaeffeMccabe <67243893+TheRaeffeMccabe@users.noreply.github.com>
2022-08-30 14:56:36 +1200

    Add files via upload
    Finished Player Array

--- Avatar.js
+++ Avatar.js
@@ -1,24 +1,16 @@
 class Player {
 
     constructor(PLAYER_IMG, x, y, h, w, xSpeed, ySpeed) {
-
+        
         this.x = x;
-
         this.y = y;
-
         this.w = w;
-
         this.h = h;
-
         this.xSpeed = xSpeed;
-
         this.ySpeed = ySpeed;
 
-
-
         PLAYER_IMG = new Image();
-
-        PLAYER_IMG.src = 'images/RainbowStar.png';
+        PLAYER_IMG.src = 'Images/Player Edited.png';
 
         this.src = PLAYER_IMG;
 
@@ -32,3 +24,30 @@ class Player {
 
     } //draws the player on the canvas
 }
+
+
+playerMove()
+
+    {
+
+    if (leftKeyPress){
+        playerXpos -= playerXspeed;
+    }
+
+    if (rightKeyPress){
+        playerXpos += playerXspeed;
+    }
+
+    if (upKeyPress){
+        playerYpos -= playerYspeed;
+    }
+    
+    if (leftKeyPress){
+        playerYpos += playerYspeed;
+    }
+
+    if (enterKeyPress){
+        pass = true;
+    }
+
+    }// End Function
